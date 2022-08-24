c4900a8ba36512d32a22fc35beb967b5ab2312b8
Sudhir Yelikar <sudhir.yelikar@thoughtworks.com>
2022-08-24 19:29:52 +0530

    simplify workflow code

--- src/workflow/WorkflowOne.ts
+++ src/workflow/WorkflowOne.ts
@@ -14,14 +14,12 @@ async function Activity3(input: number): Promise<number> {
 
 export async function WorkflowOne(input: number): Promise<number> {
     let state = 0;
-    const activity1Output = await Activity1(input);
-    state += activity1Output;
 
-    const activity2Output = await Activity2(input);
-    state += activity2Output;
+    state += await Activity1(input);
 
-    const activity3Output = await Activity3(input);
-    state += activity3Output;
+    state += await Activity2(input);
+
+    state += await Activity3(input);
 
     return state;
 }
